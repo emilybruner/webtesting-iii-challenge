0f27e9e7bee54086594564d72c2cf55ed0a47ee7
Emily <bruner.emilym@gmail.com>
2019-12-16 18:09:53 -0500

    wrote 5 tests

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,15 @@
-// Test away!
+import React from 'react';
+import { render } from "@testing-library/react";
+import Display from './Display';
+
+test('dashboard renders without crashing', () => {
+    render(<Display />)
+})
+
+test('cannot be closed or open if it is locked', () => {
+    const {getByText, rerender} = render(<Display locked />);
+    getByText(/Locked/i);
+
+    rerender(<Display closed />);
+    getByText(/Closed/i)
+})
